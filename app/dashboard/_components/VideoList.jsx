bb8e7f0008301665548e9ac2234e015a05e77d36
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-11-11 20:46:30 +0530

    Update VideoList.jsx

--- app/dashboard/_components/VideoList.jsx
+++ app/dashboard/_components/VideoList.jsx
@@ -13,7 +13,7 @@ function VideoList({ videoList }) {
     };
 
     return (
-        <div className='mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7'>
+        <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-7'>
             {videoList?.map((video, index) => (
                 <div
                     key={index}
